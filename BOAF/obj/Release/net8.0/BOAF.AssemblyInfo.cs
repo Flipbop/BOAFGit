7e52dbcc32b0bcf859a092cf066c942f957b7758
Flipbop <151550611+Flipbop@users.noreply.github.com>
2026-02-01 20:44:51 -0700

    0.5.0

--- BOAF/obj/Release/net8.0/BOAF.AssemblyInfo.cs
+++ BOAF/obj/Release/net8.0/BOAF.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BOAF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.5.0+520c7112f74c9413f099971e802de2f61bde40f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.5.0+f987f87a32fd4b7e9e228f9494a3860050b7e6d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BOAF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BOAF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.5.0.0")]
